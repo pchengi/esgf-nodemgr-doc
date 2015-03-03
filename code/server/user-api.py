3e7bf974ccd96d2fc0c3bc852f38f873943abca1
sashakames <sasha@llnl.gov>
2015-03-03 15:44:11 -0800

    working on it

---
+++ code/server/user-api.py
@@ -0,0 +1,32 @@
+import nodemgr.nodemgr.simplequeue
+import os, json
+
+
+
+REPO_DIR_PATH = "/esg/config/node-mgr-repo.json"
+
+repo_obj = []
+
+def init():
+    json.dumps
+
+# this gets called
+def init_proj(project):
+    repo_objs
+    
+
+
+def init_directory(application, project):
+
+    outf = open(
+
+json.dumps(
+
+def put_file(application, project, name, data):
+
+    
+    
+
+def get_path(application, project, name):
+
+    return
