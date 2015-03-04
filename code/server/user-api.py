0f09c9946b3a48e084bc2ff21179db3a6d10fa36
sashakames <sasha@llnl.gov>
2015-03-03 16:28:25 -0800

    more functionality

--- code/server/user-api.py
+++ code/server/user-api.py
@@ -2,25 +2,60 @@ import nodemgr.nodemgr.simplequeue
 import os, json
 
 
+#INSTALL Needed to setup empty json for bootstrapping
 
 REPO_DIR_PATH = "/esg/config/node-mgr-repo.json"
 
-repo_obj = []
+
 
 def init():
-    json.dumps
+    f = open(REPO_DIR_PATH)
+    ret = json.loads(f.read())
+    f.close()
+    return ret
 
-# this gets called
-def init_proj(project):
-    repo_objs
+def write_out():
+
+    outf = open(REPO_DIR_PATH, "w")
     
+    dat = json.dumps(repo_obj)
 
+    outf.write(dat)
+    outf.close()
+
+    #  tasKQ migrate task
+
+repo_obj = init()
+
+# this gets called once per project
+
+def init_proj(project):
+    tmpd = {}
+    tmpd["project"] = project
+    tmpd["applications"] = {}
+   
+    repo_obj.append(tmpd)
+    
+    write_out()
+
+    
 
 def init_directory(application, project):
 
-    outf = open(
+    for n in repo_obj:
+        if n["project"] == project:
 
-json.dumps(
+            app_list = n["applications"]
+            
+            tmpd = {}
+            tmpd["name"] = application
+            tmpd["files"] = []
+            
+            app_list.append(tmpd)
+    write_out()
+
+    
+    
 
 def put_file(application, project, name, data):
 
